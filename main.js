a2136a7f973af7a7e7c03f79731cdc2b627b7948
gqueico <gqueico@gmail.com>
2024-07-05 06:04:15 -0300

    :recycle: Refactor main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
 
 let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
@@ -13,22 +13,17 @@ document.addEventListener('keydown', handleResetKeydown)
 function handleTryClick(event) {
   event.preventDefault()
 
-  const inputNumber = document.querySelector("#inputNumber")
+  const inputNumber = document.querySelector('#inputNumber')
 
   if(Number(inputNumber.value) == randomNumber) {
     toggleScreen()
-    screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas!`
+    screen2.querySelector('h2').innerText = `Acertou em ${xAttempts} tentativas!`
   }
 
-  if(inputNumber.value != "") {
-    inputNumber.value = ""
+  if(inputNumber.value != '') {
+    inputNumber.value = ''
     xAttempts++
   }
-}
-
-function toggleScreen() {
-  screen1.classList.toggle("hide")
-  screen2.classList.toggle("hide")
 }
 
 function handleResetClick() {
@@ -38,7 +33,12 @@ function handleResetClick() {
 }
 
 function handleResetKeydown(event) {
-  if(event.key == 'Enter' && screen1.classList.contains("hide")) {
+  if(event.key == 'Enter' && screen1.classList.contains('hide')) {
     handleResetClick()
   }
+}
+
+function toggleScreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
 }
